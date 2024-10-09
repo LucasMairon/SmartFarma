4d1a4a9f1cda2ee6a263a1b8553af4ae80392b10
lidianacostasouza <lidianacostasouza@gmail.com>
2024-10-09 17:12:02 +0000

    configurações iniciais do backend

---
+++ backend/config/settings/production.py
@@ -0,0 +1,9 @@
+from .base import *
+from .base import env
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
