e76409aab422ea9b946ab5daa244acefb256cfa2
dayvisonmsilva <dayvisoneryc@gmail.com>
2024-10-14 16:40:54 -0300

    Navbar and Footer construction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./components/pages/Home/Home";
 import Login from "./components/pages/Login/Login";
 import Comprar from "./components/pages/Comprar/Comprar";
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar" // cabeçalho
+import Footer from "./components/layout/Footer" // rodapé
+
 
 // Digite rafce (React Arrow Function Component Export)
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/Comprar">Comprar</Link>
-        <Link to="/Login">Login</Link>
-      </div>
+      < Navbar/>
       <Container customClass="content">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -22,7 +21,7 @@ function App() {
           <Route path="/Login" element={<Login />} />
         </Routes>
       </Container>
-      <p>footer</p>
+      < Footer/> 
     </Router>
   );
 }
---
+++ frontend/src/components/layout/Navbar.jsx
@@ -0,0 +1,30 @@
+import styles from "./Navbar.module.css";
+import logo from "../../img/painkiller.png";
+
+import { Link } from "react-router-dom";
+import Container from "./Container";
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <Container>
+        <Link to="/">
+          <img src={logo} />
+        </Link>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/Comprar">Comprar</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/Login">Login</Link>
+          </li>
+        </ul>
+      </Container>
+    </nav>
+  );
+}
+
+export default Navbar;
